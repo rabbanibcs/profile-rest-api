ef278627327eb1b312e3b45be26af429e1d5fdac
rabbanibcs <rabbanibcs@gmail.com>
2021-04-17 21:49:08 +0600

    requirements.txt file created

--- profile_project/profile_project/settings.py
+++ profile_project/profile_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profile_api.apps.ProfileApiConfig''
+    'rest_framework',
+    'rest_framework.authtoken'
+
 ]
 
 MIDDLEWARE = [
