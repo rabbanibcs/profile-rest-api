89ca5e3149c0854c3b9885db9dcaf11b08371acd
rabbanibcs <rabbanibcs@gmail.com>
2021-04-18 05:50:41 +0600

    put, patch, delete functions are added to ApiView

--- profile_project/profile_api/views.py
+++ profile_project/profile_api/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializer import HelloSerializer
+from rest_framework import status
+
 
 
 class ApiView(APIView):
 
-
-    def get(self,request):
+    serializer_class=HelloSerializer
+    def get(self,request,format=None):
 
 
         an_api=[
@@ -16,3 +19,25 @@ class ApiView(APIView):
             'uses HTTP methods as function put get delete update post'
             ]
         return Response({'massege':'Hello Api','an_api':an_api})
+
+    def post(self, request):
+
+        serializer=HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            # name=serializer.data.get('name')
+            name=serializer.data['name']
+            massage="Hello {}".format(name)
+            return Response({'massage':massage})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self,request,pk=None):
+        return Response({"method":"put"})
+    def patch(self,request,pk=None):
+        return Response({"method":"patch"})
+
+    def delete(self,request,pk=None):
+        return Response({"method":"delete"})
+
+      
