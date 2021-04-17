f4f9d829a7ca4023e7b4b7b68e3a391daa97781d
rabbanibcs <rabbanibcs@gmail.com>
2021-04-18 01:47:09 +0600

    custom user, manager added

--- profile_project/profile_api/manager.py
+++ profile_project/profile_api/manager.py
@@ -1,20 +1,22 @@
 from django.contrib.auth.models import BaseUserManager
 
-
 class CustomUserManager(BaseUserManager):
 
     def create_user(self,email,username,password=None,**others):
 
         user=self.model(email=email,username=username)
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
+        return user
 
 
-def create_superuser(self,email,username,password=None,**others):
-    user=create_user(email,username,password)
-    user.is_staff=True
-    user.is_superuser=True
-    user.save()
+    def create_superuser(self,email,username,password=None,**others):
+        user=self.create_user(email,username,password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
 
 
 
--- profile_project/profile_api/models.py
+++ profile_project/profile_api/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 from .manager import CustomUserManager
-
+# from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 
-class CustomUser(AbstractBaseUser):
+
+
+
+
+class CustomUser(AbstractBaseUser,PermissionsMixin):
     email=models.EmailField(max_length=100, unique=True)
     username=models.CharField(max_length=200)
     first_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=100)
     is_active=models.BooleanField(default=True)
     is_staff=models.BooleanField(default=False)
+    is_superuser=models.BooleanField(default=False)
 
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=['username',]
