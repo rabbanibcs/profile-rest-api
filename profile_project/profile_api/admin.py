04136bfb1a1e202ea817117b9b7cb097ba2e6b4b
rabbanibcs <rabbanibcs@gmail.com>
2021-04-18 02:17:14 +0600

    a little customization of admin site

--- profile_project/profile_api/admin.py
+++ profile_project/profile_api/admin.py
@@ -3,3 +3,9 @@ from .models import CustomUser
 
 
 admin.site.register(CustomUser)
+
+
+
+admin.site.index_title='Rest_API_Project'
+admin.site.site_title='Admin Page'
+admin.site.site_header='Welcome Admin'
